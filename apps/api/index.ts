92189acba875cf98aef11ddfc583eebc91efb558
Paulo Filho <lpbalves7filho@gmail.com>
2025-11-18 20:37:03 -0300

    feat(infra-4): add elysia app

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+import { Elysia } from "elysia";
+
+new Elysia()
+  .get("/", "Hello Elysia")
+  .get("/user/:id", ({ params: { id } }) => id)
+  .post("/form", ({ body }) => body)
+  .listen(3000);
